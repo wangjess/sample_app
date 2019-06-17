97ae2d44bb0ecf912e86c79ac8d063a5624a367a
Jessica Wang <wangjess@umich.edu>
2019-06-17 10:36:43 -0400

    Fixed my error partials :) I was doing it right the whole time. Managed to do a Flash!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Flow Video Dashboard!"
       redirect_to @user
     else
       render 'new'
