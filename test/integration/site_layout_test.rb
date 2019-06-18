40f5eb634cde78a48c7c903214a7023695811339
Jessica Wang <wangjess@umich.edu>
2019-06-18 11:52:02 -0400

    Just added Faker

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +16,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path, count: 1
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "proper behavior for logged-in users" do
+    get root_path
+    log_in_as(@user)
+    get contact_path
+    assert_select "title", full_title("Contact")
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", contact_path, count: 1
   end
 end
