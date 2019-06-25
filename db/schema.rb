29a05ca0354da97e7b8a82a5041a059e2c5bbfdc
Jessica Wang <wangjess@umich.edu>
2019-06-25 14:08:19 -0400

    passed 1 test

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_25_174324) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.string "wistia_project_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
--- test/integration/users_admin_test.rb
+++ test/integration/users_admin_test.rb
@@ -18,13 +18,18 @@ class UsersAdminTest < ActionDispatch::IntegrationTest
     log_in_as(@admin)
     get users_path(@not_admin)
     assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 
   test "non-admins can't view or edit other people" do
     log_in_as(@not_admin)
     get users_path(@not_admin)
+    follow_redirect!
     assert_template '/'
     get users_path
+    follow_redirect!
     assert_template '/'
   end
 end
