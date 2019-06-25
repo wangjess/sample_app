13b4ea27aa4b38d76e3568f58ebf56fdaba3e869
Jessica Wang <wangjess@umich.edu>
2019-06-25 11:02:20 -0400

    OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_202513) do
+ActiveRecord::Schema.define(version: 2019_06_24_231900) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_202513) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.integer "wistia_project"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
