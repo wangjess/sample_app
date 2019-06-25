0afc0473fee21a276c30d63f9d1ddef72a5c2a54
Jessica Wang <wangjess@umich.edu>
2019-06-25 15:50:18 -0400

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users do
-    member do
-      put 'set_wistia_project_ID'
-    end
-  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
