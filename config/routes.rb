1fcfb6351f068e9cc83723bbf886cd411300d0f9
Jessica Wang <wangjess@umich.edu>
2019-06-26 14:08:08 -0400

    added resources user

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
