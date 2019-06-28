16edb651f62f8d0a779c61c1644f0aabc29b123d
Jessica Wang <wangjess@umich.edu>
2019-06-28 15:52:59 -0400

    pre-share

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,14 +15,12 @@ class StaticPagesController < ApplicationController
     auth_token = "b85eb878c603fbe6f87bb758ca5cffd93dbdd14d26fabe3174706116bd3912a3"
     request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}"
     @response = HTTP.get(request).body
-    list = []
-    # get embed code for each video using the hashed_id
+    @videos = []
+    # get embed code for each video using the hashed_id, put in list
     JSON.parse(@response)['medias'].each do |p|
       puts p["hashed_id"]
-      list.append(HTTP.get("http://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8").body)
+      @videos.append(HTTP.get("http://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8").body)
     end
-    @videos = list
-    puts @videos
   end
 
   def statistics
