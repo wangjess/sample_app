76b857f78187307e1a7b0065e24d462697720ec2
Jessica Wang <wangjess@umich.edu>
2019-06-28 16:55:53 -0400

    videos rendered

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,12 +14,12 @@ class StaticPagesController < ApplicationController
     project_id = @current_user.wistia_project_id
     auth_token = "b85eb878c603fbe6f87bb758ca5cffd93dbdd14d26fabe3174706116bd3912a3"
     request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}"
+
     @response = HTTP.get(request).body
-    @videos = []
+
     # get embed code for each video using the hashed_id, put in list
-    JSON.parse(@response)['medias'].each do |p|
-      puts p["hashed_id"]
-      @videos.append(HTTP.get("http://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8").body)
+    @video_iframe_urls = JSON.parse(@response)['medias'].map do |p|
+      "https://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8"
     end
   end
 
