54a57e8782ed9dba1230dbb512d5aa24b680fefb
Jessica Wang <wangjess@umich.edu>
2019-07-01 13:36:53 -0400

    need to write a test for this

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,6 +17,13 @@ class StaticPagesController < ApplicationController
 
     @response = HTTP.get(request).body
 
+    # handle errors: not 200 OK
+    puts HTTP.get(request).status
+
+    # if !HTTP.get(request).status:
+    #   render text: "ok";
+    # end
+
     # get embed code for each video using the hashed_id, put in list
     @video_iframe_urls = JSON.parse(@response)['medias'].map do |p|
       "https://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8"
