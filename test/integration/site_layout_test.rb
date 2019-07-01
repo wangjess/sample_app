72165a33adfb0c4f0644819b1b628d9b679e1437
Jessica Wang <wangjess@umich.edu>
2019-07-01 13:40:31 -0400

    fixed

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -23,8 +23,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     get contact_path
     assert_select "title", full_title("Contact")
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", root_path, count: 1
     assert_select "a[href=?]", about_path, count: 1
     assert_select "a[href=?]", contact_path, count: 1
   end
