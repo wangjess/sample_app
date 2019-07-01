2e8872b865e9f441c481b6617568542383bca680
Jessica Wang <wangjess@umich.edu>
2019-07-01 14:56:33 -0400

    styling better

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,16 +13,11 @@ class StaticPagesController < ApplicationController
     @current_user = current_user
     project_id = @current_user.wistia_project_id
     auth_token = "b85eb878c603fbe6f87bb758ca5cffd93dbdd14d26fabe3174706116bd3912a3"
-    request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}"
+    uri = URI("https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}")
+    res = Net::HTTP.get_response(uri)
 
-    @response = HTTP.get(request).body
-
-    # handle errors: not 200 OK
-    puts HTTP.get(request).status
-
-    # if !HTTP.get(request).status:
-    #   render text: "ok";
-    # end
+    # Status
+    puts res.code       # => '200'
 
     # get embed code for each video using the hashed_id, put in list
     @video_iframe_urls = JSON.parse(@response)['medias'].map do |p|
