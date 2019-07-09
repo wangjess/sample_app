fd0d898ec907b929b6e5f671f28eee4d7ec7cefe
Jessica Wang <wangjess@umich.edu>
2019-07-09 14:03:47 -0400

    almost done with your videos page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,6 +15,9 @@ class StaticPagesController < ApplicationController
     auth_token = "b85eb878c603fbe6f87bb758ca5cffd93dbdd14d26fabe3174706116bd3912a3"
     request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}"
 
+    media_ids = []
+    @each_video_stats = []
+
     @response = HTTP.get(request)
     @project_name = JSON.parse(@response)["name"]
 
@@ -27,14 +30,22 @@ class StaticPagesController < ApplicationController
 
     @response = HTTP.get(request).body # didnt get body to handle errors
 
-    # get embed code for each video using the hashed_id, put in list
+    # 1: get embed code for each video using the hashed_id, put in list
     @video_iframe_urls = JSON.parse(@response)['medias'].map do |p|
+      # get the media id for each video for 3
+      media_ids.push(p['id'])
       "https://fast.wistia.com/embed/iframe/#{p["hashed_id"]}?version=v1&controlsVisibleOnLoad=true&playerColor=aae3d8"
     end
 
-    # get title for each video, put in list
+    # 2: get title for each video, put in list
     @video_names = JSON.parse(@response)['medias'].map do |p|
       p["name"]
+    end
+
+    # 3: get individual stats for each video using the media_ids for each video, put in list
+    media_ids.each do |p|
+      request = "https://api.wistia.com/v1/stats/medias/#{p}.json?api_password=#{auth_token}"
+      @each_video_stats.push(JSON.parse(HTTP.get(request).body))
     end
   end
 
