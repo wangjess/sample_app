1f8e21d433d69ac4ab76c416f1546e2f194444b7
Jessica Wang <wangjess@umich.edu>
2019-07-15 17:03:55 -0400

    used .env to hide auth token

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,8 +12,7 @@ class StaticPagesController < ApplicationController
     # get current_user's wistia_project_id & authorization token
     @current_user = current_user
     project_id = @current_user.wistia_project_id
-    auth_token = "b85eb878c603fbe6f87bb758ca5cffd93dbdd14d26fabe3174706116bd3912a3"
-    request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{auth_token}"
+    request = "https://api.wistia.com/v1/projects/#{project_id}.json?api_password=#{ENV['AUTH_TOKEN']}"
 
     media_ids = []
     @each_video_stats = []
@@ -44,7 +43,7 @@ class StaticPagesController < ApplicationController
 
     # 3: get individual stats for each video using the media_ids for each video, put in list
     media_ids.each do |p|
-      request = "https://api.wistia.com/v1/stats/medias/#{p}.json?api_password=#{auth_token}"
+      request = "https://api.wistia.com/v1/stats/medias/#{p}.json?api_password=#{ENV['AUTH_TOKEN']}"
       # use intermediate hash variable to convert to minutes
       @hash = JSON.parse(HTTP.get(request).body)
       @hash["hours_watched"] = @hash["hours_watched"] * 60
