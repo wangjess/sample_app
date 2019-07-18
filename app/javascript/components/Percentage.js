8e321d55e3b207d18edda36e675ab83d1ef23c62
Jessica Wang <wangjess@umich.edu>
2019-07-18 14:56:44 -0400

    some dumbass javascript_Pack_tag error

---
+++ app/javascript/components/Percentage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Percentage extends React.Component {
+  render () {
+    console.log(this.props);
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Percentage
