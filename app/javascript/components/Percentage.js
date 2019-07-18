3c3427ba03c7dc8ae13774044454bbacff51ea4c
Jessica Wang <wangjess@umich.edu>
2019-07-18 19:02:32 -0400

    for some reason i cant import other stuff in react wtf

--- app/javascript/components/Percentage.js
+++ app/javascript/components/Percentage.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'app/assets/stylesheets/styles.css';
 class Percentage extends React.Component {
   render () {
     console.log(this.props);
     return (
       <React.Fragment>
+        <h1>PERCENTAGE ICON HERE:</h1>
+        <p>Percentages here</p>
       </React.Fragment>
     );
   }
