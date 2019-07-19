a99e33b0fcbce46d18cc98ecadfba30117ead700
Jessica Wang <wangjess@umich.edu>
2019-07-19 14:49:10 -0400

    FUCKING WORK

--- app/javascript/components/VideoPercent.js
+++ app/javascript/components/VideoPercent.js
@@ -4,13 +4,13 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import '../../assets/stylesheets/styles.css';
 class VideoPercent extends React.Component {
   render () {
-    console.log("HEY YOU YOU SHOULD SEE THIS IN CONSOLE")
+    console.log("HEY YOU YOU SHOULD SEE THIS IN CONSOLE IF REACT COMPONENT WORKS")
     console.log(this.props);
     return (
       <React.Fragment>
         <h1>PERCENTAGE ICON HERE:</h1>
         <p>Percentages here</p>
-        {/* <CircularProgressbar value={this.props} text={`${this.props}%`} />; */}
+        <CircularProgressbar value={this.props} text={`${this.props}%`} />;
       </React.Fragment>
     );
   }
