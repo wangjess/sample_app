6728dfdd24a90ddb205b305ae0a5d0bc316bb9ec
Jessica Wang <wangjess@umich.edu>
2019-07-19 14:27:48 -0400

    push to test on heroku

--- app/javascript/components/VideoPercent.js
+++ app/javascript/components/VideoPercent.js
@@ -4,12 +4,14 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import '../../assets/stylesheets/styles.css';
 class VideoPercent extends React.Component {
   render () {
+    console.log("HEY BUTT FUCK")
     console.log(this.props);
     return (
       <React.Fragment>
         console.log("HELLO")
         <h1>PERCENTAGE ICON HERE:</h1>
         <p>Percentages here</p>
+        <CircularProgressbar value={this.props} text={`${percentage}%`} />;
       </React.Fragment>
     );
   }
