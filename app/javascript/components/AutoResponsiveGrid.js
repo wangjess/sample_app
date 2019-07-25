7d088ba330ec5d761974e281b04d2ce682538dbe
Jessica Wang <wangjess@umich.edu>
2019-07-25 15:21:00 -0400

    going to jump ship

--- app/javascript/components/AutoResponsiveGrid.js
+++ app/javascript/components/AutoResponsiveGrid.js
@@ -1,11 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
 import AutoResponsive from 'autoresponsive-react';
+// @ts-ignore
 class AutoResponsiveGrid extends React.Component {
   render() {
     console.log(AutoResponsive)
     return (
       <AutoResponsive ref="container">
+        FUCK
       </AutoResponsive>
     );
   }
